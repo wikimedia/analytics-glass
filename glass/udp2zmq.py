3e2c5c650d98ee78d3e5adc37082d3b0f74a2cf9
Ori Livneh <ori.livneh@gmail.com>
2012-11-08 21:48:33 +0000

    Re-parse MW log => EventLog

--- glass/udp2zmq.py
+++ glass/udp2zmq.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from urlparse import parse_qsl
+from datetime import datetime
+
 from gevent import queue, server, spawn
 from gevent_zeromq import zmq
 
@@ -7,10 +10,42 @@ from gevent_zeromq import zmq
 q = queue.Queue()
 
 
+def format_ts(ts):
+    return datetime.fromtimestamp(int(ts)).strftime('%Y-%m-%dT%H:%M:%S')
+
+def munge_mw(logline):
+    seq_id, db, qs = logline.split()
+    event = dict(parse_qsl(qs))
+    ts = format_ts(event.pop('timestamp'))
+    return '?_db=%s&_id=%s mediawiki %s %s' % (db, qs.split('=', 1)[1], seq_id, ts)
+
+
+stream = []
+
+for line in stream:
+    if 'event_id' in line:
+        try:
+            line = mungw_mw(line)
+        except (ValueError, KeyError):
+            pass
+
+
 class DatagramRouter(server.DatagramServer):
     def handle(self, data, address):
+        for line in data.strip().split('\n'):
+            q.put(line.strip())
+
+
+class ErrorLogRouter(server.DatagramServer):
+    def handle(self, data, address):
         for line in data.split('\n'):
-            q.put(data)
+            if 'event_id' in line:
+                try:
+                    line = munge_mw(line)
+                except (ValueError, KeyError):
+                    pass
+                else:
+                    q.put(line.strip())
 
 
 def zmq_worker(host='*', port=8006):
@@ -19,10 +54,13 @@ def zmq_worker(host='*', port=8006):
     sock.bind('tcp://%s:%d' % (host, port))
 
     while 1:
-        sock.send(q.get())
+        line = q.get()
+        # XXX: Current only listens to enwiki data
+        if line.startswith('?_db=enwiki'):
+            sock.send(line)
 
 
 if __name__ == '__main__':
-    DatagramRouter(':8421').start()
+    ErrorLogRouter(':8421').start()
     DatagramRouter(':8422').start()
     spawn(zmq_worker).join()
